f8ac21b0d4c734a23b9b71a9839d8f6929db7ba5
ViktoriiaKharchenko <ivanovad86@gmail.com>
2020-09-03 17:45:34 +0300

    authorization lines removed

--- InsuranceDatabase/Controllers/CategoriesController.cs
+++ InsuranceDatabase/Controllers/CategoriesController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using InsuranceDatabase;
 using Microsoft.AspNetCore.Http;
 using System.IO;
-using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Authorization;
 
 namespace InsuranceDatabase.Controllers
 {
@@ -50,7 +50,7 @@ namespace InsuranceDatabase.Controllers
         }
 
         // GET: Categories/Create
-        [Authorize(Policy = "RequireBrokerRole")]
+      //  [Authorize(Policy = "RequireBrokerRole")]
         public IActionResult Create()
         {
             return View();
@@ -73,7 +73,7 @@ namespace InsuranceDatabase.Controllers
         }
 
         // GET: Categories/Edit/5
-        [Authorize(Policy = "RequireBrokerRole")]
+        //[Authorize(Policy = "RequireBrokerRole")]
         public async Task<IActionResult> Edit(int? id)
         {
             ViewBag.CategoryName = _context.Categories.Find(id).Category;
@@ -127,7 +127,7 @@ namespace InsuranceDatabase.Controllers
         }
 
         // GET: Categories/Delete/5
-        [Authorize(Policy = "RequireBrokerRole")]
+        //[Authorize(Policy = "RequireBrokerRole")]
         public async Task<IActionResult> Delete(int? id)
         {
             ViewBag.CategoryName = _context.Categories.Find(id).Category;
